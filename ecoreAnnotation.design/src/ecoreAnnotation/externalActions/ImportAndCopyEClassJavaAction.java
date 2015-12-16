b44765b7cb01ff245cbb94d5ca28a20f229c150e
kahina <kahina.hacid@gmail.com>
2015-12-16 23:26:29 +0100

    adding cop class feature

--- ecoreAnnotation.design/src/ecoreAnnotation/externalActions/ImportAndCopyEClassJavaAction.java
+++ ecoreAnnotation.design/src/ecoreAnnotation/externalActions/ImportAndCopyEClassJavaAction.java
@@ -1,14 +1,21 @@
 package ecoreAnnotation.externalActions;
 
 import java.util.Collection;
+
+import org.eclipse.emf.ecoretools.design.service.*;
+
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.sirius.business.api.session.SessionManager;
 import org.eclipse.sirius.diagram.business.internal.metamodel.spec.DSemanticDiagramSpec;
 import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
+import org.eclipse.sirius.viewpoint.DRepresentationElement;
+import org.eclipse.sirius.viewpoint.DSemanticDecorator;
 
 import ecoreAnnotation.AnnotationModel;
 
@@ -32,8 +39,18 @@ public class ImportAndCopyEClassJavaAction implements IExternalJavaAction {
 		AnnotationModel model = (AnnotationModel) parameters.get("annotationModel");
 		List<EClass> classes = (List<EClass>) parameters.get("classes");
 		
-		model.getClasses().add(EcoreUtil.copy(classes.get(0)));
+		//model.getClasses().add(EcoreUtil.copy(classes.get(0)));
 		
-		//DSemanticDiagramSpec containerView = (DSemanticDiagramSpec) parameters.get("containerView");
+	EClass tmp = EcoreUtil.copy(classes.get(0));
+	DSemanticDiagramSpec containerView = (DSemanticDiagramSpec) parameters.get("containerView");
+	//	containerView.getRepresentationElements().removeAll(classes);
+	DesignServices designServices = new DesignServices();
+	designServices.paste(model, tmp, containerView, containerView);
+	
+	
+	//DesignServices.createView(classes.get(0), containerView, SessionManager.INSTANCE.getSession(model), "containerView");
+	//EList<EObject> tmp= containerView.eContents();
+
+			
 	}
 }
